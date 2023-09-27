56a319f3a25dde87daa8fb32e2bd8dde42b2dc80
David Sánchez <davidslt+git@pm.me>
2023-09-27 21:53:49 +0100

    build: patch shebangs, remove unneeded lines

--- pkgs/nr-otel-collector/sources.nix
+++ pkgs/nr-otel-collector/sources.nix
@@ -19,16 +19,17 @@ in
     };
     nativeBuildInputs = with pkgs; [gnumake go] ++ [ocb];
     buildPhase = ''
+      # script run by make needs the correct bash location
+      patchShebangs ./scripts/build.sh
+
       export HOME=$TMPDIR
       chmod -R u+w .
       make generate-sources
-      cat distributions/nr-otel-collector/_build/build.log
     '';
     installPhase = ''
       # Remove log files as they make the build non-reproducible (contain dates)
       rm -rf distributions/nr-otel-collector/_build/build.log
       cp -r distributions/nr-otel-collector/_build/ $out
-      ls -lah $out
     '';
     outputHashAlgo = "sha256";
     outputHashMode = "recursive";
