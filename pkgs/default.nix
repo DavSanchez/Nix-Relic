50aa76213041a2a87088d9d5d82f904e8071938b
David Sánchez <davidslt+git@pm.me>
2024-10-24 00:35:05 +0100

    refactor: nr-otel-collector, update

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,29 +1,6 @@
 # You can build the below packages using 'nix build .#example'
-{pkgs}: rec {
-  infrastructure-agent = pkgs.callPackage ./infrastructure-agent.nix {};
-  ocb = pkgs.callPackage ./ocb.nix {};
-
-  nr-otel-collector = pkgs.callPackage ./nr-otel-collector {
-    # nr-otel-collector needs a specific version of ocb at this moment, hence this hack
-    ocb = let
-      version = "0.86.0";
-      src = pkgs.fetchFromGitHub {
-        owner = "open-telemetry";
-        repo = "opentelemetry-collector";
-        rev = "cmd/builder/v${version}";
-        hash = "sha256-Ucp00OjyPtHA6so/NOzTLtPSuhXwz6A2708w2WIZb/E=";
-        fetchSubmodules = true;
-      };
-    in
-      # For why this was needed, see
-      # https://discourse.nixos.org/t/inconsistent-vendoring-in-buildgomodule-when-overriding-source/9225/6
-      ocb.override rec {
-        buildGoModule = args:
-          pkgs.buildGoModule (args
-            // {
-              inherit src version;
-              vendorHash = "sha256-MTwD9xkrq3EudppLSoONgcPCBWlbSmaODLH9NtYgVOk=";
-            });
-      };
-  };
+{ pkgs }:
+{
+  infrastructure-agent = pkgs.callPackage ./infrastructure-agent.nix { };
+  nr-otel-collector = pkgs.callPackage ./nr-otel-collector.nix { };
 }
---
+++ pkgs/nr-otel-collector.nix
@@ -0,0 +1,63 @@
+{
+  lib,
+  pkgs,
+  buildGoModule,
+  fetchFromGitHub,
+  opentelemetry-collector-builder,
+}:
+let
+  pinnedOcbVersion = opentelemetry-collector-builder.override {
+    buildGoModule = previousArgs: pkgs.buildGoModule (previousArgs // {
+      version = "0.108.0";
+      src = previousArgs.src // {
+        sha256 = lib.fakeSha256;
+      };
+    });
+  };
+in
+buildGoModule rec {
+  pname = "nr-otel-collector";
+  version = "0.8.3";
+
+  src = fetchFromGitHub {
+    owner = "newrelic";
+    repo = "opentelemetry-collector-releases";
+    rev = "${pname}-${version}";
+    sha256 = "sha256-tUuNqaI92M2JVTI5Y5tEAe0ocPlNGxt+9v5Y84pGnrs=";
+  };
+
+  # Generate the distribution sources
+  preConfigure = ''
+    patchShebangs ./scripts/build.sh
+
+    export HOME=$TMPDIR
+    chmod -R u+w .
+
+    ./scripts/build.sh -d "nr-otel-collector" -s true -b "${pinnedOcbVersion}/bin/ocb" -g "${pkgs.go}/bin/go"
+  '';
+
+  # The generated distribution sources end up in this location.
+  # This is the actual Go module we will build
+  modRoot = "distributions/nr-otel-collector/_build";
+
+  vendorHash = "sha256-CV+Azc/dEYLun1LxAixtC7s7Z+ijTdwGwrjo8UHPAYw=";
+
+  ldflags = [
+    "-s"
+    "-w"
+  ];
+
+  # The TestGenerateAndCompile tests download new dependencies for a modified go.mod. Nix doesn't allow network access so skipping.
+  checkFlags = [ "-skip TestValidateConfigs" ];
+
+  CGO_ENABLED = "0";
+
+  meta = with lib; {
+    description = "The New Relic distribution of the OpenTelemetry Collector";
+    homepage = "https://github.com/newrelic/opentelemetry-collector-releases.git";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ DavSanchez ];
+    mainProgram = "nr-otel-collector";
+    platforms = platforms.all;
+  };
+}
